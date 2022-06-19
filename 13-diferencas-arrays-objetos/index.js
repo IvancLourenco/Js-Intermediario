d846c23af89cbf0242bd51d086bae66c9f319c87
IvancLourenco <ivancampos039@gmail.com>
2022-06-19 11:34:56 -0300

    novos arquivos

---
+++ 13-diferencas-arrays-objetos/index.js
@@ -0,0 +1,17 @@
+let frutas = ["uva", "banana", "melão"]
+
+let videoGame = {
+    nome: "Xbox",
+    valor: 3000
+}
+
+let videoGame2 = {
+    nome: "Playstation",
+    valor: 5000
+}
+
+let videosGames= [videoGame, videoGame2]
+
+console.log(videosGames)
+
+console.log(videosGames[1].nome)
