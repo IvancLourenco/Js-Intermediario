819ae2ec832f2579f40299ecc3e0ff138f875997
ivanclourenco <ivancampos039@gmail.com>
2022-06-23 21:06:07 -0300

    Arquivos novos

---
+++ 16-selecionando-elementos-dom/index.js
@@ -0,0 +1,20 @@
+//console.log(document)
+
+//let p = document.getElementById("paragrafo1")
+
+//console.log(p)
+
+ //BUSCA TODAS AS TAGS DA MESMA CLASSE
+//let inputsPorClasse = document.getElementsByClassName("tag-de-input") 
+
+//console.log(inputsPorClasse[0])
+
+//BUSCA AS TAGS PELO NOME
+//let inputPoTag = document.getElementsByTagName("h1")
+
+//console.log(inputPoTag)
+
+//faz uma pesquisa pelo name e atributo
+let inputPorName = document.querySelectorAll("input[name='email']");
+
+console.log(inputPorName[1])
